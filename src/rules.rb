f7d6b884758f11cab90abbf72154cc7637a920ac
Daniel Hoey <danielhoey1@gmail.com>
2013-07-09 22:43:58 +1000

    Automatically create build task Rejig rules test

--- src/rules.rb
+++ src/rules.rb
@@ -11,6 +11,7 @@ class Rules
     end
 
     define_clean_tasks
+    define_build_task
   end
 
   def add_rule(file, filter)
@@ -28,6 +29,10 @@ class Rules
       @file_system.remove_all(@intermediate_files)
     end
   end
+
+  def define_build_task
+    @rake_app.define_task(Rake::Task, :build => @intermediate_files)
+  end
 end
 
 
@@ -35,36 +40,53 @@ require "minitest/autorun"
 class RulesTest < Minitest::Unit::TestCase
   def setup
     @rake_app = RakeAppMock.new
+    @filter = Minitest::Mock.new
+    @file_system = Minitest::Mock.new
   end
-
-  def test_single_file_rule
-    filter = Minitest::Mock.new
-    file_system = Minitest::Mock.new
-    rules = Rules.new(@rake_app, file_system, 'index.html.erb' => filter)
-
-    task = @rake_app.tasks[0]
-    assert_equal(Rake::FileTask, task.klass)
-    assert_equal({'source/index.html' => 'source/index.html.erb'}, task.params)
-
-    filter.expect(:process, 'filter output', ['src file content'])
-    file_system.expect(:read, 'src file content', ['source/index.html.erb'])
-    mtime = Time.now
-    file_system.expect(:mtime, mtime, ['source/index.html.erb'])
-    file_system.expect(:write, nil, ['source/index.html', 'filter output', mtime])
-    task.block.call
-    [filter, file_system].each(&:verify)
-    
-    task = @rake_app.tasks[1]
-    assert_equal(Rake::Task, task.klass)
-    assert_equal(:clean_intermediate_files, task.params)
-    file_system.expect(:remove_all, nil, [['source/index.html']])
-    task.block.call
-    file_system.verify
+  
+  def make_rules(rules)
+    Rules.new(@rake_app, @file_system, rules)
   end
 
   def test_output_file_extension
     rules = Rules.new(@rake_app, nil, 'style.ext2.ext1' => nil)
     assert_equal('source/style.ext2', @rake_app.tasks[0].params.keys[0])
+  end
+
+  def test_single_file_rule
+    make_rules('index.html.erb' => @filter)
+    should_make_file_task
+    should_make_clean_task
+    should_make_build_task
+  end
+
+  def should_make_file_task
+    task = @rake_app.tasks[0]
+    assert_equal(Rake::FileTask, task.klass)
+    assert_equal({'source/index.html' => 'source/index.html.erb'}, task.params)
+
+    @filter.expect(:process, 'filter output', ['src file content'])
+    @file_system.expect(:read, 'src file content', ['source/index.html.erb'])
+    mtime = Time.now
+    @file_system.expect(:mtime, mtime, ['source/index.html.erb'])
+    @file_system.expect(:write, nil, ['source/index.html', 'filter output', mtime])
+    task.block.call
+    [@filter, @file_system].each(&:verify)
+  end
+
+  def should_make_clean_task 
+    task = @rake_app.tasks[1]
+    assert_equal(Rake::Task, task.klass)
+    assert_equal(:clean_intermediate_files, task.params)
+    @file_system.expect(:remove_all, nil, [['source/index.html']])
+    task.block.call
+    @file_system.verify
+  end
+
+  def should_make_build_task
+    task = @rake_app.tasks[2]
+    assert_equal(Rake::Task, task.klass)
+    assert_equal({:build => ['source/index.html']}, task.params)
   end
 
   class RakeAppMock
@@ -75,7 +97,8 @@ class RulesTest < Minitest::Unit::TestCase
     end
 
     def define_task(klass, params, &block)
-      @tasks << OpenStruct.new(:klass => klass, :params => params, :block => Proc.new(&block))
+      block = Proc.new(&block) if block
+      @tasks << OpenStruct.new(:klass => klass, :params => params, :block => block)
     end
   end
 end
--- test/run.rb
+++ test/run.rb
@@ -3,7 +3,7 @@ require "minitest/autorun"
 class FunctionalTest < Minitest::Unit::TestCase
   def test_webrake
     Dir.chdir(File.dirname(__FILE__))
-    system("rake make_clean");
+    system("rake build_clean");
     assert_equal('', `diff source expected/source`)
     
     system("rake clean_intermediate_files");
