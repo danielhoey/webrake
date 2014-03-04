b648ca26b2825967667159dd5c1cd587ec4c06ef
Daniel Hoey <danielhoey1@gmail.com>
2014-03-04 17:18:30 +1100

    Import paths for Filters::Less

--- lib/filters/less.rb
+++ lib/filters/less.rb
@@ -2,13 +2,13 @@ require_relative 'base'
 
 module Webrake::Filters
 class Less < Base
-  def initialize
+  def initialize(*import_paths)
     require 'less'
+    @import_paths = import_paths
   end
 
   def transform(content, front_matter, modify_time)
-    ::Less::Parser.new.parse(content).to_css
-      #.to_css(:compress => true)
+    ::Less::Parser.new(:paths => @import_paths).parse(content).to_css
   end
 
   def default_output_file_extension
